526fba5f292c70a522f4d045e6d59cb4b4517751
Mateusz <mateusz.lesniewski1992@gmail.com>
2023-06-26 11:20:41 +0200

    ping command with author

--- bot.py
+++ bot.py
@@ -1,4 +1,16 @@
 import discord
 from discord.ext import commands
 
- 
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("Bot is connected to discord") 
+  
+@bot.command()
+async def ping(ctx):
+    to = ctx.author
+    await ctx.author.send(f"Pong you too! {to}")
+  
+bot.run("MTEyMjExNTM3MDgyNDc2NTUxMA.GbOsmP.CXOVRc4k1FGsW6GR0Y2BcKOop45sF4KruqcuXE")
